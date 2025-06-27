51582f99cd1a932573755f041dfb5b52c982c80f
Arian Amirpour <ariyan.amirpour@gmail.com>
2025-06-27 13:52:40 +0330

    Get Data from Database

--- todoapp/views.py
+++ todoapp/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages
+from .models import todo
 # Create your views here.
 
-def home(requset):
-    return render(requset, 'todoapp/todo.html', {})
+def home(request):
+    if request.method == 'POST':
+        task = request.POST.get('task')
+        new_todo = todo(user=request.user, todo_name=task)
+        new_todo.save()
+        return redirect('home-page')  # <-- Add this line
+
+    all_todos = todo.objects.filter(user=request.user)
+    context = {
+        'todos': all_todos
+    }
+    return render(request, 'todoapp/todo.html', context)
 
 def register(request):
     if request.method == 'POST':
